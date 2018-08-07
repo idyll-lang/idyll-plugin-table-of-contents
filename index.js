50b3de416c507bd5d1e4f242971fc8a0abf9d2f4
Christian Frisson <christian.frisson@gmail.com>
2018-08-07 17:31:52 -0600

    Fix table-of-contents DOM

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ const AST = require('idyll-ast');
 
 module.exports = (ast) => {
     let headings = AST.getNodesByName(ast, 'h1');
-    let tags = ['Table of Contents'];
+    let tags = [];
     headings.forEach((node) => {
         let text = AST.getText(node);
         let href = '#' + text.replace(/['"]+/g, '');
         let element = AST.createNode('a', {href}, [text])
         tags.push(AST.createNode('li', {}, [element]));
     });
-    let tagNames = AST.createNode('h2',{id: 'tableofcontents'}, tags);
-    let list = AST.createNode('ul', {id: 'list'}, [tagNames])
+    let list = AST.createNode('ul', {id: 'list'}, tags)
     let ASTwithID = AST.modifyNodesByName(ast, 'h1', (node) => {
         node = AST.setProperty(node, 'id', AST.getText(node))
         return node;
     });
-
-    let textContainer = AST.createNode('TextContainer', {}, [list])
+    let tocTitle = AST.createNode('h1',{id: 'tableofcontents'}, ['Table Of Contents']);
+    let textContainer = AST.createNode('div', {id: 'tocContainer'}, [tocTitle,list])
     ASTwithID = AST.modifyNodesByName(ASTwithID, 'TableOfContents', (node) => {
         return textContainer;
     });
