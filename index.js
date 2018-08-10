e13fe641d2dff0ece9c1c217b008394d302652c0
Christian Frisson <christian.frisson@gmail.com>
2018-08-09 23:24:52 -0600

    Two-level table-of-contents

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const AST = require('idyll-ast');
 
 module.exports = (ast) => {
-    let headings = AST.getNodesByName(ast, 'h1');
+    let headings = AST.findNodes(ast,(node) => {
+        let type = node[0].toLowerCase();
+        return type === 'h1' || type == 'h2';
+    });
     let tags = [];
+    let h = 'h1';
+    tags['h1'] = [];
+    tags['h2'] = [];
     headings.forEach((node) => {
         let text = AST.getText(node);
         let href = '#' + text.replace(/['"]+/g, '');
-        let element = AST.createNode('a', {href}, [text])
-        tags.push(AST.createNode('li', {}, [element]));
+        let type = node[0].toLowerCase();
+        let element = AST.createNode('a', {href}, [text]);
+
+        if(h !== type && type === 'h1'){
+            tags[type].push(AST.createNode('ul', {id: 'list'}, tags['h2']));
+            tags['h2'] = [];
+        }
+        tags[type].push(AST.createNode('li', {}, [element]));
+        h = type;
     });
-    let list = AST.createNode('ul', {id: 'list'}, tags)
-    let ASTwithID = AST.modifyNodesByName(ast, 'h1', (node) => {
+    let list = AST.createNode('ul', {id: 'list'}, tags['h1'])
+    let ASTwithID = ast;
+    ASTwithID = AST.modifyNodesByName(ASTwithID, 'h1', (node) => {
+        node = AST.setProperty(node, 'id', AST.getText(node))
+        return node;
+    });
+    ASTwithID = AST.modifyNodesByName(ASTwithID, 'h2', (node) => {
         node = AST.setProperty(node, 'id', AST.getText(node))
         return node;
     });
