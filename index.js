ff27005f16c097ec1611ee68e5a5813e8ae56445
Christian Frisson <christian.frisson@gmail.com>
2018-09-26 19:05:29 -0600

    Fork AST.getText to fix header titles

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const AST = require('idyll-ast');
 
+const getText = function (node) {
+    const texts = [];
+    AST.walkNodes(AST.getChildren(node), (n) => {
+        if (typeof n === 'string') {
+            texts.push(n);
+        }
+    })
+    return texts.join('');
+}
+
 module.exports = (ast) => {
     var timeBegin = process.hrtime();
     console.log('Generating table of contents...');
-    let headings = AST.findNodes(ast,(node) => {
+    let headings = AST.findNodes(ast, (node) => {
         let type = node[0].toLowerCase();
         return type === 'h1' || type == 'h2';
     });
@@ -12,23 +22,24 @@ module.exports = (ast) => {
     tags['h1'] = [];
     tags['h2'] = [];
     headings.forEach((node) => {
-        let text = node[2][0];//AST.getText(node);
+        let text = getText(node);
         let href = '#' + text.replace(/['"]+/g, '');
         let type = node[0].toLowerCase();
-        let element = AST.createNode('a', {href}, [text]);
+        let element = AST.createNode('a', { href }, [text]);
+        console.log(node, text, href, type, element)
 
-        if(h !== type && type === 'h1'){
-            tags[type].push(AST.createNode('ul', {id: 'list'}, tags['h2']));
+        if (h !== type && type === 'h1') {
+            tags[type].push(AST.createNode('ul', { id: 'list' }, tags['h2']));
             tags['h2'] = [];
         }
         tags[type].push(AST.createNode('li', {}, [element]));
         h = type;
     });
-    if(h === 'h2' && tags['h2'] !== []){
-        tags['h1'].push(AST.createNode('ul', {id: 'list'}, tags['h2']));
+    if (h === 'h2' && tags['h2'] !== []) {
+        tags['h1'].push(AST.createNode('ul', { id: 'list' }, tags['h2']));
         tags['h2'] = [];
     }
-    let list = AST.createNode('ul', {id: 'list'}, tags['h1'])
+    let list = AST.createNode('ul', { id: 'list' }, tags['h1'])
     let ASTwithID = ast;
     ASTwithID = AST.modifyNodesByName(ASTwithID, 'h1', (node) => {
         node = AST.setProperty(node, 'id', AST.getText(node))
@@ -38,8 +49,8 @@ module.exports = (ast) => {
         node = AST.setProperty(node, 'id', AST.getText(node))
         return node;
     });
-    let tocTitle = AST.createNode('h1',{id: 'tableofcontents'}, ['Table Of Contents']);
-    let textContainer = AST.createNode('div', {id: 'tocContainer'}, [tocTitle,list])
+    let tocTitle = AST.createNode('h1', { id: 'tableofcontents' }, ['Table Of Contents']);
+    let textContainer = AST.createNode('div', { id: 'tocContainer' }, [tocTitle, list])
     ASTwithID = AST.modifyNodesByName(ASTwithID, 'TableOfContents', (node) => {
         return textContainer;
     });
